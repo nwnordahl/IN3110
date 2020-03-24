0e40d64719cc0dfa298827f68bacad9874ecf6af
nicolawn <nicolawn@student.matnat.uio.no>
2020-03-24 04:22:42 +0100

    Simple function for making journal entries

---
+++ exercises2/journal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function journal {
+# A simple journal
+    if [ "$1" == "log" ]; then
+        cut -c 9-11 journal.txt | uniq -c
+    else
+        MESSAGE="$(date +'%Y-%m-%d %H:%M:%S') - $1"
+        echo $MESSAGE 1>> journal.txt
+    fi
+}
