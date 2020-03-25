754f37e500d2a5e47774b635b5198507af29a8fc
nicolawn <nicolawn@student.matnat.uio.no>
2020-03-25 10:30:48 +0100

    Add log command (not fully working yet)

--- assignment2/track.sh
+++ assignment2/track.sh
@@ -36,13 +36,51 @@ function track {
         else
             echo "There is no active task"
         fi
+    elif [ "$1" == "log" ]; then
+        declare -i n; n=1
+        if [ "$(tail -n1 track.txt | cut -d' ' -f1)" == "LABEL" ]; then
+            while read t; do
+                if [ $n -eq $(($(wc -l track.txt | cut -d' ' -f1) - 2)) ]; then
+                    break
+                fi
+
+                if [ $((n % 4)) -eq 1 ]; then
+                    LOG_START=$(echo $t | cut -d' ' -f2-)
+                    LOG_START=$(date --date '$LOG_START' +%s) # Date converted to seconds since the UNIX epoch
+                elif [ $((n % 4)) -eq 2 ]; then
+                    LOG_NAME=$(echo $t | cut -d' ' -f2-)
+                elif [ $((n % 4)) -eq 3 ]; then
+                    LOG_STOP=$(echo $t | cut -d' ' -f2-)
+                    LOG_STOP=$(date --date '$LOG_STOP' +%s) # Date converted to seconds the UNIX epcoch
+                    LOG_TOTAL=$((LOG_START -LOG_STOP))
+                    echo ${LOG_NAME}: $LOG_TOTAL
+                fi 
+                ((n++))
+            done < track.txt
+        else
+            while read t; do
+                if [ $((n % 4)) -eq 1 ]; then
+                    LOG_START=$(echo $t | cut -d' ' -f2-)
+                    LOG_START=$(date --date '$LOG_START' +%s) # Date converted to seconds since the UNIX epoch
+                elif [ $((n % 4)) -eq 2 ]; then
+                    LOG_NAME=$(echo $t | cut -d' ' -f2-)
+                elif [ $((n % 4)) -eq 3 ]; then
+                    LOG_STOP=$(echo $t | cut -d' ' -f2-)
+                    LOG_STOP=$(date --date '$LOG_STOP' +%s) # Date converted to seconds since the UNIX epoch
+                    LOG_TOTAL=$((LOG_START - LOG_STOP))
+                    echo ${LOG_NAME}: $LOG_TOTAL
+                fi
+                ((n++))
+            done < track.txt
+        fi
     else
-	cat <<-EOF
+	cat <<-EOF # -EOF for printing indented lines
 	Usage: track <command>
 	Commands:
 	start <label> - Start tracking a new task with name <label>
 	stop - Stop tracking task
 	status - Show current status of tracking
+	log - Show time usage of tasks
 	EOF
     fi
 }
