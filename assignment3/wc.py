c69136bf6fb58e2e10e19a1a082ec913315839ee
nicolawn <nicolawn@student.matnat.uio.no>
2020-03-26 20:17:52 +0100

    finish simple file statisitics function and add more options

--- assignment3/wc.py
+++ assignment3/wc.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python3
 
 import sys
+from os import listdir
+from os.path import isfile
 
-filename = sys.argv[1]
-file = open(f'{filename}', 'r')
 
-line_counter = 0
-word_counter = 0
-letter_counter = 0
+def wc(filename):
+    line_counter = 0
+    word_counter = 0
+    letter_counter = 0
 
-for line in file:
-    word_list = line.split()
-    line_counter += 1
-    word_counter += len(word_list)
-    letter_counter += len(line)
-file.close()
+    file = open(filename, 'r')
 
-print(line_counter, word_counter, letter_counter, filename)
+    for line in file:
+        line_counter += 1
+        word_counter += len(line.split())
+        letter_counter += len(line)
+    file.close()
+
+    print(line_counter, word_counter, letter_counter, filename)
+
+
+if __name__ == "__main__":
+    option = sys.argv[1]
+
+    if option == "*":
+        files = [f for f in listdir() if isfile(f)]
+        for filename in files:
+            wc(filename)
+
+    elif option == "*.py":
+        files = [f for f in listdir() if isfile(f) and f.split('.')[-1] == "py"]
+        for filename in files:
+            wc(filename)
+
+    else:
+        filename = option
+        wc(filename)
