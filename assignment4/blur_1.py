b04e82ad0c8fd6223cb0b1aa90e35b8154fbeba2
nicolawn <nicolawn@student.matnat.uio.no>
2020-03-31 02:58:35 +0200

    minor edits

--- assignment4/blur_1.py
+++ assignment4/blur_1.py
@@ -10,17 +10,19 @@ def blur(filename):
 
     src = src.astype("uint32")  # Prevent overloading when using uint8
     src = np.pad(src, ((1, 1), (1, 1), (0, 0)), mode='edge')
-    print(src.shape)
-    for h in range(1, src.shape[0] - 2):
-        for w in range(1, src.shape[1] - 2):
-            for c in range(src.shape[2]):
-                dst[h, w, c] = (src[h, w, c] + src[h-1, w, c] + src[h+1, w, c]
-                                + src[h, w-1, c] + src[h, w+1, c]
-                                + src[h-1, w-1, c] + src[h-1, w+1, c]
-                                + src[h+1, w-1, c] + src[h+1, w+1, c])/9
 
-    dst = dst.astype("uint8")
-    cv2.imwrite(f"blurred_{filename.split('.')[0]}.jpg", dst)
+    # Calculate average of pixels nearby, creates blur effect
+    for h in range(1, src.shape[0] - 1):
+        for w in range(1, src.shape[1] - 1):
+            for c in range(src.shape[2]):
+                dst[h-1, w-1, c] = (src[h, w, c]
+                                    + src[h-1, w, c] + src[h+1, w, c]
+                                    + src[h, w-1, c] + src[h, w+1, c]
+                                    + src[h-1, w-1, c] + src[h+1, w+1, c]
+                                    + src[h-1, w+1, c] + src[h+1, w-1, c])/9
+
+    dst = dst.astype("uint8")  # Round to nearest integer
+    cv2.imwrite(f"blurred_{filename.split('.')[0]}_1.jpg", dst)
 
 
 if __name__ == "__main__":
